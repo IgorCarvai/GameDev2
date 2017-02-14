dd8e2ce7800e2bfc3a6aca99a79437d6846173a9
luc3rpi <luc3@rpi.edu>
2017-02-13 22:50:44 -0500

    Basic working inventory system

---
+++ New Unity Project/Assets/Scripts/Inventory.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+public class Inventory : MonoBehaviour{
+
+    GameObject inventoryPanel;
+    GameObject slotPanel;
+    public GameObject inventorySlot;
+    public GameObject inventoryItem;
+    ItemDatabase database;
+
+    int slotAmount;
+    public List<Item> items = new List<Item>();
+    public List<GameObject> slots = new List<GameObject>();
+
+    void Start()
+    {
+        database = GetComponent<ItemDatabase>();
+
+        slotAmount = 20;
+        inventoryPanel = GameObject.Find("Inventory Panel");
+        slotPanel = inventoryPanel.transform.FindChild("Slot Panel").gameObject;
+
+        for (int i = 0; i < slotAmount; i++)
+        {
+            items.Add(new Item());
+            slots.Add(Instantiate(inventorySlot));
+            slots[i].GetComponent<Slot>().id = i;
+            slots[i].transform.SetParent(slotPanel.transform);
+        }
+
+        AddItem(0);
+        AddItem(0);
+        AddItem(0);
+        AddItem(0);
+        AddItem(0);
+        AddItem(1);
+    }
+
+    public void AddItem(int id)
+    {
+        Item itemToAdd = database.FetchItemByID(id);
+        //already existing, and stackable
+        if (itemToAdd.Stackable && CheckIfItemExists(itemToAdd))
+        {
+            for (int i = 0; i < items.Count; i++)
+               if (items[i].ID == id)
+               {
+                    //child 0 is text obj
+                    ItemData data = slots[i].transform.GetChild(0).GetComponent<ItemData>();
+                    data.amount++;
+                    data.transform.GetChild(0).GetComponent<Text>().text = data.amount.ToString();
+                    break;
+                }
+        }
+
+
+        else //not stackable & hasn't exsisted
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                //empty item
+                if (items[i].ID == -1)
+                {
+                    items[i] = itemToAdd;
+                    GameObject itemObj = Instantiate(inventoryItem);
+                    itemObj.GetComponent<ItemData>().item = itemToAdd;
+                    itemObj.GetComponent<ItemData>().amount = 1;
+                    //slot it's attached to
+                    itemObj.GetComponent<ItemData>().slot = i;
+                    itemObj.transform.SetParent(slots[i].transform);
+                    itemObj.transform.position = Vector2.zero;
+
+                    itemObj.GetComponent<Image>().sprite = itemToAdd.Sprite;
+                    itemObj.name = itemToAdd.Title;
+
+                    break;
+                }
+            }
+        }
+    }
+
+    bool CheckIfItemExists(Item item)
+    {
+        for (int i = 0; i < items.Count; i++)
+            //already have one in inventory
+            if (items[i].ID == item.ID)
+                return true;
+        return false;
+    }
+}
