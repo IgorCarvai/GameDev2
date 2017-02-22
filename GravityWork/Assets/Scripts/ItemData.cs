cbfd18bdb8e2fec2e08d08ad1bc5a4ccc84aa358
IgorCarvai <carvai@rpi.edu>
2017-02-22 16:12:02 -0500

    Added teh Ui but it need tweeking

---
+++ GravityWork/Assets/Scripts/ItemData.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class ItemData : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerEnterHandler, IPointerExitHandler {
+
+    public Item item;
+    public int amount;
+    public int slot;
+
+    private Inventory inv;
+    private Tooltip tooltip;
+    private Vector2 offset; 
+
+
+    void Start()
+    {
+        inv = GameObject.Find("Inventory").GetComponent<Inventory>();
+        tooltip = inv.GetComponent<Tooltip>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (item != null)
+        {
+            offset = eventData.position - new Vector2(this.transform.position.x, this.transform.position.y);
+            this.transform.SetParent(this.transform.parent.parent);
+            //x, y of mouse
+            this.transform.position = eventData.position - offset;
+            GetComponent<CanvasGroup>().blocksRaycasts = false;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (item != null)
+        {
+            //x, y of mouse
+            this.transform.position = eventData.position - offset;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        this.transform.SetParent(inv.slots[slot].transform);
+        this.transform.position = inv.slots[slot].transform.position;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tooltip.Activate(item);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tooltip.Deactivate();
+    }
+}
