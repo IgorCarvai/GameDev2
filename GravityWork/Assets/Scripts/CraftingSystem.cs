3230a10fd8b4617275742f64b3ea43564af84cfc
luc3rpi <luc3@rpi.edu>
2017-03-07 13:28:45 -0500

    can't make nothing anymore
    making multiple things work occassionally

---
+++ GravityWork/Assets/Scripts/CraftingSystem.cs
@@ -0,0 +1,18 @@
+﻿ using System;
+using System.Collections;
+using System.Collections.Generic;
+using LitJson;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CraftingSystem : MonoBehaviour {
+
+    public static Dictionary<string, int> toMake = new Dictionary<string, int>();
+    private Inventory inv;
+
+    void Update()
+    {
+    }
+
+}
