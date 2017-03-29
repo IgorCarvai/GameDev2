d7ee5c020b372f199c6e7cf30a061b5587bdaf7d
IgorCarvai <carvai@rpi.edu>
2017-03-29 14:34:11 -0400

    getting stuff done :)

--- GravityWork/Assets/Scripts/TutorialScript.cs
+++ GravityWork/Assets/Scripts/TutorialScript.cs
@@ -6,13 +6,15 @@ public class TutorialScript : MonoBehaviour {
 
 
 	public GameObject MyText;
-	public GameObject Inv;
+	public Inventory inv;
 	public Camera Cam;
+	private string cpuName="";
 	private bool playerCol=false;
 
 	private Vector3 o = new Vector3(-180,0,0);
 	// Use this for initialization
 	void Start () {
+		inv = GameObject.Find("Inventory").GetComponent<Inventory>();
 	}
 	
 	// Update is called once per frame
@@ -22,16 +24,21 @@ public class TutorialScript : MonoBehaviour {
 		MyText.transform.LookAt (Cam.transform.position);
 		MyText.transform.Rotate (0, 180, 0);
 
-		if (this.name == "Mechanic") {
+		if (cpuName == "Mechanic") {
 			if (Input.GetKeyUp (KeyCode.E) && playerCol == true) {
 				MyText.SetActive (false);
 			}
 		}
-		if (this.name == "Captain") {
+		if (cpuName == "Captain") {
 
 			if (Input.GetKeyUp (KeyCode.E)) {
-				//if Inv has the item we want then load the next scene
-				Application.LoadLevel(2);
+				foreach (Item item in inv.items) { //look through entire list of items
+
+					if (item.Title == "Crystal") {
+
+						Application.LoadLevel(2);
+					}
+				}
 			}
 		}
 
@@ -39,11 +46,13 @@ public class TutorialScript : MonoBehaviour {
 	void OnTriggerExit(Collider col){
 		if (col.gameObject.tag == "Player") {
 			playerCol = false;
+			cpuName = "";
 		}
 	}
 	void OnTriggerEnter(Collider col){
 		if (col.gameObject.tag == "Player") {
 			playerCol = true;
+			cpuName = this.name;
 		}
 	}
 }
