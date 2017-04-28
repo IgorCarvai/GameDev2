20665787c552cbc148f3cd21507fea9f1328eb52
IgorCarvai <carvai@win.rpi.edu>
2017-04-28 16:31:50 -0400

    final edits

--- GravityWork/Assets/Scripts/NPCDialogue.cs
+++ GravityWork/Assets/Scripts/NPCDialogue.cs
@@ -62,7 +62,7 @@ public class NPCDialogue : MonoBehaviour
         else if (player == 3)
         {
             speaker = "Tangie";
-		} 
+		}  
 		else if (player == 4)
 		{
 			speaker = "Mechanic";
@@ -75,25 +75,25 @@ public class NPCDialogue : MonoBehaviour
                 if (textNumber == 1)
                     dialogueLine = "I was looking all over for you.";
                 if (textNumber == 2)
-                    dialogueLine = "Go and find the others, I'll meet you at the base.";
+                    dialogueLine = "Go and find the others. I'll meet you at the base.";
                 if (textNumber == 3)
-                    dialogueLine = "You still havent found anyone else.";
+                    dialogueLine = "Still haven't found the others?";
                 if (textNumber == 4)
                     dialogueLine = "We are still mising Ridi.";
 				if (textNumber == 5)
-					dialogueLine = "Find Angie then come here.";
+					dialogueLine = "Find Tangie, then come here.";
 
             }
             else if (sceneNumber >= 4)//cutscene 2
             {
 				if(textNumber == 0)
-	                dialogueLine = "Find objects around the planet and use them to craft using inventory.\nYou can find Bone Steel Wire and Mirror Shards.";
+	                dialogueLine = "Find objects around the planet and use them to craft using inventory.\n Mind finding some things for me so we can fix the ship?\n If you come across any debris, like Bones, Steel scraps, Wires, and Mirror Shards, I can probably jurryrig some parts.";
 				if (textNumber == 1)
-					dialogueLine = "Here are the recipes: \n5 Screw + 5 Wire + 3 Piston make the Engine. \n10 Screw + 10 Steel 2 Piston make the Solid Rocket Booster.\n10 Wire + 15 Screw + 10 Steel make the Fuel Tank. \n5 Steel + 10 Screw make the Radiator.";
+					dialogueLine = "Here's the compontenets we need:\n 5 Steel scraps and 1 Wire make a Piston.\n 1 Cog and 1 Steel scrps make a Screw.\n 1 Steel and 1 Bone make a Cog.";
 				if (textNumber == 2)
-					dialogueLine = "10 Mirror Shards + 5 Steel + 1 Wire make the Solar Panels.\n5 Cog + 5 Wire + 10 Steel make the Sensors.";
+					dialogueLine = "10 Mirror Shards, 5 Steel scraps, and 1 Wire make the Solar Panels.\n 5 Cogs, 5 Wires, and 10 Steel scraps make the Sensors.";
 				if (textNumber == 3)
-					dialogueLine = "5 Steel + 1 Wire make a Piston.\n1 Cog + 1 Steel Screw.\n1 Steel + 1 Bone make a Cog.";  
+					dialogueLine = "5 Screws, 5 Wires, and 3 Pistons make the Engine.\n 10 Screws, 10 Steel scraps, and 2 Pistons make the Solid Rocket Booster.\n 10 Wires, 15 Screws, and 10 Steel scraps make the Fuel Tank.\n 5 Steel scraps, and 10 Screws make the Radiator.";  
 			}
         }
 
@@ -101,16 +101,16 @@ public class NPCDialogue : MonoBehaviour
         {
 			if (sceneNumber == 3) {//world after prologue
 				if (textNumber == 1)
-					dialogueLine = "-happy as can be~ Oh hey Cappy!";
+					dialogueLine = "Sup Cappy!";
 				if (textNumber == 2)
-					dialogueLine = "Good Luck Finding others.";
+					dialogueLine = "Imma hang out in this this neato tree for a bit, but I'll be at the base soon enough. Good luck finding others.";
 				if (textNumber == 3)
-					dialogueLine = "Oh dear I hope they aren't lost";
+					dialogueLine = "Sure are taking thier sweet time reconvening. Wonder if those nerds got lost trying to find the ship.";
 				if (textNumber == 4)
-					dialogueLine = "Wheres Angie?";
+					dialogueLine = "Wheres Tangie?";
 				if (textNumber == 5)
-					dialogueLine = "Alvin is always left behind.";
-			} else if (sceneNumber == 4) {//inside spaceship
+					dialogueLine = "Seems like Avin always gets left behind.";
+			}  else if (sceneNumber == 4) {//inside spaceship
 				if (textNumber == 1)
 					dialogueLine = "Parallel Lines have so much in common.";
 				if (textNumber == 2)
@@ -132,17 +132,17 @@ public class NPCDialogue : MonoBehaviour
 				if (textNumber == 10)
 					dialogueLine = "But all I did was mist.";
 				if (textNumber == 11)
-					dialogueLine = "Whats the fastestest way of getting out of a planet?";
+					dialogueLine = "Whats the fastest way of getting out of a planet?";
 				if (textNumber == 12)
 					dialogueLine = "By finding all the pieces, building the ship and leaving.";
 				if (textNumber == 13)
 					dialogueLine = "Come on, find everything and lets go.";
 				if (textNumber == 14)
-					dialogueLine = "Come on, find everything and lets go.";
+					dialogueLine = "Come on, find everything and lets skiddaddle.";
 				if (textNumber == 15)
-					dialogueLine = "Please go and find everything so we can leave. \nPleeeeeeeaaaaaaasssseee.";
+					dialogueLine = "Please go and find everything so we can leave. \n Pleeeeeeeaaaaaaasssseee.";
 				if (textNumber == 16)
-					dialogueLine = "Ugh fine.\nI'll tell you the jokes again.";
+					dialogueLine = "Ugh fine.\n I'll tell you the jokes again.";
 				
 			}
         }
@@ -154,13 +154,13 @@ public class NPCDialogue : MonoBehaviour
                 if (textNumber == 1)
                     dialogueLine = "I could've got myself out if you let me.";
                 if (textNumber == 2)
-                    dialogueLine = "Did you find the other yet? No? Then what are you still doing here? I'll meet you at base when u do.";
+                    dialogueLine = "Did you find the others yet? No? Then what are you still doing here? I'll meet you at base when you do.";
                 if (textNumber == 3)
                     dialogueLine = "I'm busy here since no one is around. Hurry up and find the others.";
                 if (textNumber == 4)
                     dialogueLine = "We are still mising Ridi";
 				if (textNumber == 5)
-					dialogueLine = "We need the whole team. Go find Alvin quick!";
+					dialogueLine = "We need the whole team. Go find Avin.";
             }
             else if (sceneNumber == 4 )//inside spaceship
             {
@@ -179,10 +179,10 @@ public class NPCDialogue : MonoBehaviour
 					dialogueLine = "Okay we are almost ready to go. We just need to power up the ship with a crystal. Find one and come talk to me after.";
 
 				if (textNumber == 3)
-					dialogueLine = "Hey, looks like you found it. You can press I to check it out in your inventory.\nAlso since you are near me I can help you build objects. At the moment there is nothing that needs to be built. But go ahead and try to check your inventory.";
+					dialogueLine = "Hey, looks like you found it. You can press I to check it out in your inventory.\n Also since you are near me I can help you build objects. At the moment there is nothing that needs to be built. But go ahead and try to check your inventory.";
 
 				if (textNumber == 4)
-					dialogueLine = "Pretty cool huh. Okay now give me the crystal and I'll put it on the ship.\n(Hint: Press E)";
+					dialogueLine = "Pretty cool huh. Okay now give me the crystal and I'll put it on the ship.\n (Hint: Press E)";
 
 				if (textNumber == 5)
 					dialogueLine = "We will be all set pretty soon. Go ahead and tell the Pilot that we are all good to go.";
@@ -225,3 +225,4 @@ public class NPCDialogue : MonoBehaviour
 
     }
 }
+
