e86553afd1337cea16fc5775b3b9f25715f2a6b5
IgorCarvai <carvai@rpi.edu>
2017-05-02 18:10:54 -0400

    added arrows that point to the base

--- GravityWork/Assets/Scripts/FollowScript.cs
+++ GravityWork/Assets/Scripts/FollowScript.cs
@@ -6,15 +6,26 @@ public class FollowScript : MonoBehaviour {
 	public Transform player;
 	// Use this for initialization
 	void Start () {
-
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (this.tag == "Character Dialogue") {
 			transform.LookAt (transform.position + player.rotation * Vector3.forward, player.rotation * Vector3.up);
+		}if (this.tag == "FollowArrow") {
+			
+			Vector3 relativePos = player.position - transform.position;
+			Quaternion rotation = Quaternion.LookRotation(relativePos);
+			transform.rotation = rotation;
+			transform.localEulerAngles = new Vector3(
+				90f,
+				transform.localEulerAngles.y,
+				transform.localEulerAngles.z
+			);
+
 		} else {
 			transform.LookAt (player.transform, player.rotation * Vector3.up);
 		}
 	}
+
 }
