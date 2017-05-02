0a6ccfaf0f48baf8c111f63946ea91c5e13601f7
luc3rpi <luc3@rpi.edu>
2017-05-01 20:30:58 -0400

    added a loading scene
    use to load between cutscenes/ especially between scene 3/4

---
+++ GravityWork/Assets/Scripts/LoadingS.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LoadingS : MonoBehaviour
+{
+
+    public float loadingTime;
+    public Image loadingbar;
+    public int scenetoLoad;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        loadingbar.fillAmount = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (loadingbar.fillAmount <= 1)
+        {
+            loadingbar.fillAmount += 1.0f / loadingTime * Time.deltaTime;
+
+        }
+
+        if (loadingbar.fillAmount == 1.0f)
+        {
+            Application.LoadLevel(scenetoLoad);
+        }
+    }
+}
